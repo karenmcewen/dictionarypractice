42db6904e4aa36268ec1bde1767adb702b6ba21a
Karen McEwen <karenmcewen.engineer@gmail.com>
2019-11-30 11:44:29 -0500

    adding to dict

--- dictionarypractice.py
+++ dictionarypractice.py
@@ -5,3 +5,9 @@
 
 alien_0 = {'color': 'green', 'points': 5}
 print(alien_0['color'])
+
+# dictionaries are dynamic - can add key:value pairs
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print(alien_0)
+
