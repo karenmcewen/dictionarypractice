bc7e331577ada8d5cc3280a9bd4da5118f3b5a8a
Karen McEwen <karenmcewen.engineer@gmail.com>
2019-11-30 11:36:22 -0500

    first commmit

---
+++ dictionarypractice.py
@@ -0,0 +1,7 @@
+# Dictionary practice chapter 6 Python Crash Course
+# a dictionary uses curly brackets {} and key:value pairs
+# keys - can be string, list, even another dictionary
+# values - any object in python can be a value in a dictionary
+
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0['color'])
