baec8f537478b06f8b226ca3660103d1949b9091
Karen McEwen <karenmcewen.engineer@gmail.com>
2019-12-01 20:25:49 -0500

    moving alien

--- dictionarypractice.py
+++ dictionarypractice.py
@@ -9,5 +9,19 @@ print(alien_0['color'])
 # dictionaries are dynamic - can add key:value pairs
 alien_0['x_position'] = 0
 alien_0['y_position'] = 25
+alien_0['speed'] = 'medium'
 print(alien_0)
 
+# now let's move our alien to the right!
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    # alien must be fast
+    x_increment = 3
+
+# the new position is the old plus the increment
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+
+print(f"The alien's new position is {alien_0['x_position']}")
